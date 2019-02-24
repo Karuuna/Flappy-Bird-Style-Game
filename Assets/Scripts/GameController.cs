7684cfa18cebe9ab7493ee5f6b5d4d22cb33932b
Karuuna <45571423+Karuuna@users.noreply.github.com>
2019-02-24 23:07:41 +0100

    V1 > Before Scrolling Background

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public static GameController instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+            {
+            instance = this;    
+            }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
